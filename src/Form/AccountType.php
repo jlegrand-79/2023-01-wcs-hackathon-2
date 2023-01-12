813a96347f4ad93ba2cd6ab7e3eac072b1ee420d
Jérôme Legrand <jerome.legrand.79@gmail.com>
2023-01-12 17:14:57 +0100

    adding account crud

---
+++ src/Form/AccountType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            // ->add('roles')
+            ->add('password')
+            // ->add('grantedUser')
+            // ->add('community')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Account::class,
+        ]);
+    }
+}
